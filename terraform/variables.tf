3f0eab094921acd3f34484512ac09e0428623747
Antew01 <136913357+Antew01@users.noreply.github.com>
2024-11-28 15:50:40 +0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "yc_iam_token" {
+  type = string
+}
+
+variable "yc_cloud_id" {
+  type = string
+}
+
+variable "yc_folder_id" {
+  type = string
+}
+
+variable "yc_postgresql_version" {
+  type    = string
+  default = "15"
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+  sensitive = true
+}
